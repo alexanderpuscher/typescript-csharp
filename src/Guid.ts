d321d560c21960b0e427d0be5d1ad5962f389889
Alexander Puscher <alexander.puscher@immowelt.de>
2020-04-20 15:04:04 +0200

    changed folder to dist; renamed typings

--- src/Guid.ts
+++ src/Guid.ts
@@ -7,18 +7,9 @@ export class Guid {
   public static Empty: string = '00000000-0000-0000-0000-000000000000';
 
   public static newGuid(): string {
-    let d = new Date().getTime();
-    let d2 = (performance && performance.now && performance.now() * 1000) || 0;
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-      let r = Math.random() * 16;
-      if (d > 0) {
-        r = (d + r) % 16 | 0;
-        d = Math.floor(d / 16);
-      } else {
-        r = (d2 + r) % 16 | 0;
-        d2 = Math.floor(d2 / 16);
-      }
-      return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16);
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
     });
   }
 }
