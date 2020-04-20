42d24c02ffd7c0c589242f86340c2dc61579739e
Alexander Puscher <alexander.puscher@immowelt.de>
2020-04-20 15:04:21 +0200

    refactored nodejs sample

--- samples/typescript-csharp-nodejs/app.ts
+++ samples/typescript-csharp-nodejs/app.ts
@@ -1,10 +1,11 @@
 import express from 'express';
+import { Guid } from 'typescript-csharp';
 
 const app = express();
 const port = 3000;
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send(Guid.newGuid());
 });
 app.listen(port, function () {
     console.log('Example app listening on port 3000!');
